1b3018c372afba6a1170e8cb200261b983a21c55
HarshitTaq <harshit.rajput@taqtics.co>
2025-08-06 20:25:07 +0530

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -265,7 +265,7 @@ if uploaded_file is not None:
         fig_bell.add_vrect(x0=0, x1=60, fillcolor="#FF0000", opacity=0.2, line_width=0, annotation_text="Below Expectations", annotation_position="top left")
         fig_bell.add_vrect(x0=60.1, x1=75.5, fillcolor="#FFC0CB", opacity=0.2, line_width=0, annotation_text="N/I", annotation_position="top left")
         fig_bell.add_vrect(x0=75.6, x1=95, fillcolor="#32CD32", opacity=0.2, line_width=0, annotation_text="M/E", annotation_position="top left")
-        fig_bell.add_vrect(x0=95.1, x1=100, fillcolor="#006400", opacity=0.7, line_width=0, annotation_text="Outstanding", annotation_position="top left")
+        fig_bell.add_vrect(x0=95.1, x1=100, fillcolor="#006400", opacity=0.4, line_width=0, annotation_text="Outstanding", annotation_position="top left")
 
         fig_bell.add_annotation(x=30, y=max(pdf_y)/1.5, text="Non-Performers", showarrow=False, font=dict(size=12, color="black"))
         fig_bell.add_annotation(x=75, y=max(pdf_y)/.5, text="Developing Performers", showarrow=False, font=dict(size=12, color="black"))
